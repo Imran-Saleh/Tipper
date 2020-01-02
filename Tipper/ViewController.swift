a85fde12de531685f74c1099e70b1e0f8710572c
Imran Saleh <imsaleh99@gmail.com>
2020-01-02 12:49:44 -0800

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Imran Saleh on 1/2/20.
+//  Copyright © 2020 Imran Saleh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
